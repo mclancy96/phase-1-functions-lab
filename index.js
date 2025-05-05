a52144219e4b597f2e5e3605f3640cb073eb1ac6
Mike <mclancy96@gmail.com>
2025-05-05 15:14:24 -0400

    CalculatesFarePrice returning correct amount

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ const distanceFromHqInFeet = (blockNumber) => {
 const distanceTravelledInFeet = (start, end) => {
     return Math.abs(start - end) * blockDistanceInFeet
 }
+
+const calculatesFarePrice = (start, end) => {
+    const distance = distanceTravelledInFeet(start, end)
+    if (distance > 2500) {
+        return 'cannot travel that far';
+    } else if (distance > 2000) {
+        return 25;
+    } else {
+        return 0.02 * Math.max(0, (distance - 400));
+    }
+}
